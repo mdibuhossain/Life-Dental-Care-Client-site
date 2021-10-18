d5080e23be3569380ebd84f731a117592cb7657a
Ibrahim H <mohammad.ihakash@gmail.com>
2021-10-18 18:41:19 +0600

    react-tilt added

--- src/components/Service.js
+++ src/components/Service.js
@@ -6,10 +6,10 @@ const Service = () => {
     const { serviceList } = useAPI();
     console.log(serviceList);
     return (
-        <div>
-            <h4 className="text-blue-500 font-semibold">What we do</h4>
-            <h1 className="text-4xl font-bold">Our services</h1>
-            <div className="service-container gap-3">
+        <div className="md:w-10/12 mx-auto">
+            {/* <h4 className="text-blue-500 font-semibold">What we do</h4> */}
+            <h1 className="text-4xl font-bold text-center">Our services</h1>
+            <div className="service-container gap-3 flex flex-wrap justify-center">
                 {
                     serviceList.map(item => <ServiceItem key={item.id} service={item} />)
                 }
