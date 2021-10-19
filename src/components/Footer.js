49288ac0d685368ceebefef0fc23e3f7eeba2e43
Ibrahim H <mohammad.ihakash@gmail.com>
2021-10-19 18:30:16 +0600

    profile page retouch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsFacebook, BsTwitter, BsLinkedin, BsDribbble } from 'react-icons/bs';
 import { navList } from '../Utilities/utilities'
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="sm:w-9/12 w-11/12 mx-auto flex flex-col justify-center text-center">
-                <div className="my-5 flex flex-wrap justify-center items-center">
+        <div className="footer py-5">
+            <div className="sm:w-9/12 w-11/12 mx-auto grid sm:grid-cols-3 text-center">
+                <div className="my-5">
+                    <Link to="/">
+                        <img className="w-52 sm:mx-auto" src="/assets/img/logo.svg" alt="logo" />
+                    </Link>
+                    <div className="flex sm:justify-center items-center my-5 text-2xl">
+                        <BsFacebook className="mx-2" />
+                        <BsTwitter className="mx-2" />
+                        <BsLinkedin className="mx-2" />
+                        <BsDribbble className="mx-2" />
+                    </div>
+                </div>
+                <div className="my-5 flex flex-col text-left">
+                    <p className="mb-2">Pages</p>
                     {
                         navList.map(item => {
                             return (
                                 <Link
                                     key={item.name}
                                     to={item.to}
-                                    className="text-gray-300 hover:text-white px-3 py-2 text-sm font-medium"
+                                    className="text-gray-300 hover:text-white py-1 text-sm font-medium"
                                 >
                                     {item.name}
                                 </Link>
@@ -21,15 +34,19 @@ const Footer = () => {
                         })
                     }
                 </div>
-                <div className="my-5">
-                    <Link to="/">
-                        <img className="w-48 mx-auto" src="/assets/img/logo.svg" alt="logo" />
-                    </Link>
+                <div className="my-5 text-left">
+                    <p className="mb-2">Address</p>
+                    <div className="font-semibold text-gray-300">
+                        <p>Life Dental Care</p>
+                        <p>#2/32, Nikunjo-2</p>
+                        <p>Dhaka, Bangladesh</p>
+                    </div>
                 </div>
-                <div className="text-gray-400 mb-5 grid md:grid-cols-2">
-                    <p>Developed by Ibrahim Hossain</p>
-                    <p>&copy; All rights reserved by Life Dental Care</p>
-                </div>
+            </div>
+            <div className="sm:w-9/12 w-11/12 text-gray-400 mt-5 mx-auto text-center">
+                <p>Developed by Ibrahim Hossain</p>
+                <hr className="border-gray-600 my-2" />
+                <p>&copy; 2021 All rights reserved by Life Dental Care</p>
             </div>
         </div>
     );
